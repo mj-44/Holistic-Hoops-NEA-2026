a5f29cfe4dfbb58631a6a5fca63279203e0f375c
mj-44 <matthewmutinda44@gmail.com>
2026-03-14 10:37:40 +0000

    Fix variable name for theme colors

--- rangerumble.py
+++ rangerumble.py
@@ -148,8 +148,8 @@ class RangeRumble(tk.Frame):
         theme_manager.register(self.apply_theme)
 
     def apply_theme(self):
-        c = theme_manager.colours
-        self.configure(background=c["background"])
+        colour = theme_manager.colours
+        self.configure(background=colour["background"])
 
     def record_make(self):
         #Reording a made shot
