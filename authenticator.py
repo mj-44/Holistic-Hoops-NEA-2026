5da0696e935e6ef8607e48e57bac010a7bef1785
mj-44 <matthewmutinda44@gmail.com>
2026-03-14 10:15:40 +0000

    Enhance code comments for clarity in authenticator.py
    Added comments to clarify the purpose of various functions related to password handling and user validation.

--- authenticator.py
+++ authenticator.py
@@ -1,9 +1,10 @@
 import hashlib
 
+#Hashing the password for the password storage to be secure in the database
 def hashPassword(password):
-    #use SHA-256 for secure storage
     return hashlib.sha256(password.encode()).hexdigest()
 
+#Validating the username entry against certain conditions to ensure it is of a suitable format to be entered into the database
 def validateUsername(username):
     if not username:
         return False, "Username cannot be empty"
@@ -19,6 +20,7 @@ def validateUsername(username):
     
     return True, ""
 
+#Validates the users entry of password when they register to ensure its suitable
 def validatePassword(password):
     if not password:
         return False, "Password cannot be empty"
@@ -28,26 +30,32 @@ def validatePassword(password):
     
     return True, ""
 
+#Verifies the users entry when they use the login page
 def verifyLogin(username, password):
     from database import get_user
-
+    #Checks if the username entered by the user matches a name in the database which should be fetched with the get user function
     user = get_user(username)
     if not user:
         return False, "Invalid username or password", None
     
+    #chekcs if the password the user has entered matches the one stored in the database by hashing the users input
+    #and checking it against the hashed stored password in the database
     hashedPassword = hashPassword(password)
     if hashedPassword != user["password"]:
-        return False, "Invalid username or password"
+        return False, "Invalid username or password", None
     
     return True, "Login successful", user
 
+#verifies the security answer the user has entered on the forgot password page
 def verifySecurityAnswer(username, securityAnswer):
     from database import get_user
 
+    #Checks if the username they have entered is actually in the database
     user = get_user(username)
     if not user:
         return False, "Username note found", None
     
+    #Hashes the answer from the user and checks it against the hashed security answer in the database to check if they match
     hashedAnswer = hashPassword(securityAnswer.lower())
     if hashedAnswer != user["security_answer"]:
         return False, "Incorrect security answer", None
