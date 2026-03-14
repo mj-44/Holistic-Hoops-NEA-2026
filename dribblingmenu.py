f8f23e4205e1c1892ca86da1b05bf7a298574237
mj-44 <matthewmutinda44@gmail.com>
2026-03-14 10:21:09 +0000

    Refactor comments for clarity in DribblingMenu

--- dribblingmenu.py
+++ dribblingmenu.py
@@ -2,17 +2,18 @@ import tkinter as tk
 from colourscheme import ButtonStyle, StyledLabel, BG_COLOUR, ACCENT_COLOUR, TEXT_COLOUR, HOVER_COLOUR, create_frame
 from thememanager import theme_manager
 
+#Creating the class for the dribbling menu
 class DribblingMenu(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent, background = BG_COLOUR)
         self.controller = controller
         theme_manager.register(lambda: self.configure(background=theme_manager.colours["background"]))
 
-        #Container frame to centralise content
+        #Container frame to centralise content on the menu for the user
         main_frame = create_frame(self)
         main_frame.place(relx = 0.5, rely = 0.5, anchor = "center")
 
-        #Creating the title for the dribbling menu
+        #Titling the page as the "Dribbling Menu"
         title = StyledLabel(
             main_frame,
             text = "Dribbling Menu",
@@ -22,34 +23,34 @@ class DribblingMenu(tk.Frame):
         )
         title.pack(pady=(0,50))
 
-        #Button to Fullcourt Weave Drill page
-        alley_drill_button = ButtonStyle(
+        #Creating button to allow the user to access the full court weave drill
+        fullcourtweave_drill_button = ButtonStyle(
             main_frame,
             text = "Fullcourt Weave Drill",
             command = lambda: controller.show_page("FullcourtWeaveDrill"),
             width = 20
         )
-        alley_drill_button.pack(pady=10)
+        fullcourtweave_drill_button.pack(pady=10)
 
-        #Button to Spin Move Drill page
-        cone_touch_finishing_button = ButtonStyle(
+        #Another button allowing the user to access the spin move drill
+        spinmove_drill_button = ButtonStyle(
             main_frame,
             text = "Spin Move Drill",
             command = lambda: controller.show_page("SpinMoveDrill"),
             width = 20
         )
-        cone_touch_finishing_button.pack(pady=10)
+        spinmove_drill_button.pack(pady=10)
 
-        #Button to Driving lay Drill page
-        fresno_attack_button = ButtonStyle(
+        #Final button on the page (for now) allowing the user to access the driving layup drill
+        drivinglay_drill_button = ButtonStyle(
             main_frame,
             text = "Driving Lay Drill",
             command = lambda: controller.show_page("DrivingLayDrill"),
             width = 20
         )
-        fresno_attack_button.pack(pady=10)
+        drivinglay_drill_button.pack(pady=10)
 
-        #Button to return to the training menu when needed
+        #Button allowing the user to return to the training menu when the user is done with this menu
         backButton = ButtonStyle(
             main_frame,
             text = "Back to Training Menu",
@@ -58,8 +59,9 @@ class DribblingMenu(tk.Frame):
         )
         backButton.pack(pady=10)
 
+        #Automatically applying the theme the user has chosen to this page
         theme_manager.register(self.apply_theme)
 
     def apply_theme(self):
-        c = theme_manager.colours
-        self.configure(background=c["background"])
+        colour = theme_manager.colours
+        self.configure(colour["background"])
